951da5836545a62affd7ee2ede6fa123fdc4cffc
Ethan Weiner <ethanweiner20@gmail.com>
2022-04-06 15:04:37 -0400

    Set up navigation bar

--- app.rb
+++ app.rb
@@ -1,9 +1,77 @@
 require 'bundler/setup'
-
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
 get '/' do
-  erb :home
+  redirect '/plants'
+end
+
+# AUTHENTICATION
+
+get '/signup' do
+end
+
+post '/users' do
+  redirect '/plants'
+end
+
+get '/login' do
+end
+
+get '/users' do
+  redirect '/plants'
+end
+
+# SEARCH ALL PLANTS
+
+# Render plant search form (no list)
+get '/plants' do
+  erb :search
+end
+
+# Render plant search form and list of matching plants
+get '/plants/:name_query' do
+  @plants = []
+  erb :search
+end
+
+# CUSTOM PLANTS
+
+# Add plant to a user's custom plants
+get '/users/plants/new' do
+end
+
+post '/users/plants' do
+end
+
+# Edit a custom plant
+get '/users/plants/:id/edit' do
+end
+
+post '/users/plants/:id' do
+end
+
+# Inventories
+
+# Manage inventories
+get '/users/:username/inventories/new' do
+end
+
+post '/users/:username/inventories' do
+end
+
+# Render list of plants in user's inventory
+get '/users/:username/inventories/:inventory_name' do
+  erb :inventory
+end
+
+# Add a plant to a user's inventory
+post '/users/:username/inventories/:inventory_name' do
+end
+
+# SETTINGS
+
+get '/settings' do
+  erb :settings
 end
