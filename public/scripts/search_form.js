87007c42589870b62d9451a9a1158fad9bda3bd8
Ethan Weiner <ethanweiner20@gmail.com>
2022-04-08 17:35:42 -0400

    Text and checkbox input types

--- public/scripts/search_form.js
+++ public/scripts/search_form.js
@@ -1,25 +1,46 @@
-const show_filters_html = `
-Show More Filters
-<svg class="filters-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-</svg>
-`;
-
-const hide_filters_html = `
-Hide Filters
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
-</svg>
-`;
-
 $(document).ready(function () {
-  $("#filters").on("show.bs.collapse", function () {
-    const button = $('button[data-bs-target="#filters"]');
-    button.html(hide_filters_html);
+  function addTextInput(name) {
+    const inputName = name.replace(/\s/g, "");
+
+    $("div.filters").append(`
+      <div class="col-sm-6 col-md-4 col-lg-3 col-12">
+        <label for="${inputName}">${name}</label>
+        <input type="text" name="${inputName}" id="${inputName}" class="form-control" placeholder="${inputName}" />
+      </div>`);
+  }
+
+  function checkboxHTML(name, option) {
+    return `
+    <div class="form-check">
+      <input type="checkbox" class="form-check-input" id="${option}" name="${name}[]" value="${option}"/>
+      <label class="form-check-label" for="${option}">${option}</label>
+    </div>
+    `;
+  }
+
+  function addCheckboxInput(name, options) {
+    const inputName = name.replace(/\s/g, "");
+
+    const html = `
+    <div class="col-sm-6 col-md-4 col-lg-3 col-12">
+      <label>${name}</label>
+      ${options.map((value) => checkboxHTML(inputName, value)).join("")}
+    </div>
+    `;
+
+    $("div.filters").append(html);
+  }
+
+  $("a.add-text-input").click(function (event) {
+    event.preventDefault();
+    const inputName = $(this).attr("data-input-name");
+    addTextInput(inputName);
   });
 
-  $("#filters").on("hide.bs.collapse", function () {
-    const button = $('button[data-bs-target="#filters"]');
-    button.html(show_filters_html);
+  $("a.add-checkbox-input").click(function (event) {
+    event.preventDefault();
+    const inputName = $(this).attr("data-input-name");
+    const options = $(this).attr("data-options").split(",");
+    addCheckboxInput(inputName, options);
   });
 });
