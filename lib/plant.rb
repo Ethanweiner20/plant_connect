379fcc2bd9d6f15fc40069f66b5848e42b81339a
Ethan Weiner <ethanweiner20@gmail.com>
2022-04-07 21:53:21 -0400

    Wikimedia image API implementation

--- lib/plant.rb
+++ lib/plant.rb
@@ -1,24 +1,27 @@
 require 'uri'
 require 'net/http'
 require 'json'
+require 'pry'
 
 # Stores the data for a given plant
 class Plant
   attr_reader :data
 
+  IMAGE_SEARCH_URI = "https://commons.wikimedia.org/w/api.php"
+
   WIKIMEDIA_IMAGE_REQUEST_PARAMS = {
     action: 'query',
     generator: 'images',
     prop: 'imageinfo',
-    ginlimit: '1',
+    gimlimit: '1',
     redirects: '1',
-    iiprop: 'timestamp|user|userid|comment|canonicaltitle|url|size|dimensions|\
-    sha1|mime|thumbmime|mediatype|bitdepth',
+    iiprop: 'url',
     format: 'json'
   }
 
   def initialize(data)
     @data = data
+    @image_src = nil
   end
 
   def [](key)
@@ -29,11 +32,10 @@ class Plant
   # Used in various display areas
   def color; end
 
-  # Find a Wikipedia image for the plant
-  def image_src
-    uri = URI("https://commons.wikimedia.org/w/api.php")
+  def find_image_source(titles)
+    uri = URI(IMAGE_SEARCH_URI)
     params = WIKIMEDIA_IMAGE_REQUEST_PARAMS.dup
-    params[:titles] = data[:scientific_name] || data[:common_name]
+    params[:titles] = titles.join('|')
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
     if response.is_a?(Net::HTTPSuccess)
@@ -41,8 +43,13 @@ class Plant
     end
   end
 
+  # Find a Wikipedia image for the plant
+  def image_src
+    find_image_source([data["Scientific Name"], data["Common Name"]])
+  end
+
   def extract_src(response)
-    json = JSON.parse(response.body)
-    json["query"]["pages"].values[0]["imageinfo"][0]["url"]
+    query = JSON.parse(response.body)["query"]
+    return query["pages"].values[0]["imageinfo"][0]["url"] if query
   end
 end
